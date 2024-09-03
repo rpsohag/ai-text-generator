7a8cd34beb324747a7a1723753c26ec188c83185
rpsohag <rpsohag.bd@gmail.com>
2024-09-03 14:44:09 +0600

    install fresh react project with tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Index from "./chat/Index"
+
+
+function App() {
+  return (
+    <>
+     <Index/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/chat/Index.jsx
@@ -0,0 +1,9 @@
+
+
+const Index = () => {
+  return (
+    <div>ChatGPT</div>
+  )
+}
+
+export default Index
